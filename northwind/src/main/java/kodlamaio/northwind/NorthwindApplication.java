1113e755df456f618a55643659a0d08a36e202e9
Muhammed Koçak <78095933+Muhammed-Kocak@users.noreply.github.com>
2021-05-22 23:56:07 +0300

    22.05.2021 - Lecture day ('Result' structure)

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -2,12 +2,26 @@ package kodlamaio.northwind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2 //swaggeri başlatan annotacion
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindApplication.class, args);
 	}
 
+	@Bean //uygulama başladığı anda spring boot beani gördüğü anda belleğe yerleştiriyor.
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))                                     
+          .build();                                           
+    }
 }
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,23 +6,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.ErrorResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
-@Service //business'da ki servisimiz demek istiyoruz.
+@Service // business'da ki servisimiz demek istiyoruz.
 public class ProductManager implements ProductService {
 
 	private ProductDao productDao;
-	
-	@Autowired //otomatik dependency yapıyor ve bize instance veriyor.
+
+	@Autowired // otomatik dependency yapıyor ve bize instance veriyor.
 	public ProductManager(ProductDao productDao) {
 		super();
 		this.productDao = productDao;
 	}
 
 	@Override
-	public List<Product> getAll() {
-		return productDao.findAll();
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data Listelendi");
+	}
+
+	@Override
+	public Result add(Product product) {
+		var result = this.productDao.findById(product.getId());
+
+		if (result.isEmpty()) {
+			return new ErrorResult("Aynı isimde bir ürün mevcut");
+		} else {
+			this.productDao.save(product);
+			return new SuccessResult("Ürün eklendi");
+		}
+	}
+
+	@Override
+	public Result update(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result delete(Product product) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
